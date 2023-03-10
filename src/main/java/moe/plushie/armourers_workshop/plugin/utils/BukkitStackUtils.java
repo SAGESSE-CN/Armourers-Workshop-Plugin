6a0d8b5ac4a897542437dabf7487b61236a5c3fc
Happy-FZM <501787394@qq.com>
2023-03-10 18:21:28 +0800

    Implement ItemStack wrap and unwrap

--- src/main/java/moe/plushie/armourers_workshop/plugin/utils/BukkitStackUtils.java
+++ src/main/java/moe/plushie/armourers_workshop/plugin/utils/BukkitStackUtils.java
@@ -1,8 +1,14 @@
 package moe.plushie.armourers_workshop.plugin.utils;
 
 import moe.plushie.armourers_workshop.plugin.api.ItemStack;
+import net.querz.nbt.io.SNBTUtil;
+import net.querz.nbt.tag.*;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
+
+import java.io.IOException;
+import java.util.Map;
 
 public class BukkitStackUtils {
 
@@ -10,16 +16,38 @@ public class BukkitStackUtils {
         if (itemStack == null || itemStack.getAmount() <= 0) {
             return ItemStack.EMPTY;
         }
-        // TODO: IMPL
-        return null;
+        ItemStack item = ItemStack.EMPTY;
+        try {
+            NBTEditor.NBTCompound itemCompound = NBTEditor.getEmptyNBTCompound();
+            itemCompound.set(itemStack.getType().getKey().getNamespace() + ":" + itemStack.getType().getKey().getKey(), "id");
+            NBTEditor.NBTCompound tag = NBTEditor.getNBTCompound(itemStack, "tag");
+            if (tag != null) {
+                itemCompound.set(tag, "tag");
+            }
+            itemCompound.set((byte) itemStack.getAmount(), "Count");
+            CompoundTag tag1 = (CompoundTag) SNBTUtil.fromSNBT(itemCompound.toJson());
+            item = new ItemStack(tag1);
+        } catch (Exception ignored) {
+        }
+        return item;
     }
 
     public static org.bukkit.inventory.ItemStack unwrap(ItemStack itemStack) {
         if (itemStack.isEmpty()) {
             return null;
         }
-        // TODO: IMPL
-        return null;
+        org.bukkit.inventory.ItemStack bukkitItem = new org.bukkit.inventory.ItemStack(Material.PAPER);
+        try {
+            CompoundTag tag = new CompoundTag();
+            itemStack.save(tag);
+            String nbt = SNBTUtil.toSNBT(tag);
+            org.bukkit.inventory.ItemStack itemFromTag = NBTEditor.getItemFromTag(NBTEditor.getNBTCompound(nbt));
+            if (itemFromTag != null) {
+                bukkitItem = itemFromTag;
+            }
+        } catch (Exception ignored) {
+        }
+        return bukkitItem;
     }
 
     public static void giveItemTo(ItemStack itemStack, Player player) {
