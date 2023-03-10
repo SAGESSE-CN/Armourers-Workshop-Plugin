ae144967b7782bc46406a23365c7391e5eece505
Happy-FZM <501787394@qq.com>
2023-03-11 00:42:37 +0800

    Implement when close wardrobe clear menu view

--- src/main/java/moe/plushie/armourers_workshop/plugin/packet/PacketListener.java
+++ src/main/java/moe/plushie/armourers_workshop/plugin/packet/PacketListener.java
@@ -23,26 +23,30 @@ public class PacketListener extends PacketAdapter {
 
     @Override
     public void onPacketReceiving(PacketEvent event) {
-        if (event.getPacketType() != PacketType.Play.Client.WINDOW_CLICK) {
-            return;
+        if (event.getPacketType().equals(PacketType.Play.Client.WINDOW_CLICK)) {
+            Player player = event.getPlayer();
+            if (player == null) {
+                return;
+            }
+            PacketContainer packet = event.getPacket();
+            int menuId = packet.getIntegers().read(0);
+            Menu menu = MenuManager.getMenu(menuId);
+            if (menu == null) {
+                return;
+            }
+            int slot = packet.getIntegers().read(1);
+            int button = packet.getIntegers().read(2);
+            if (slot < 0) {
+                return; // is drop, ignored.
+            }
+            String type = ((Enum<?>) packet.getStructures().read(1).getHandle()).name();
+            menu.handSlotClick(slot, button, ClickType.valueOf(type), player);
+            event.setCancelled(true);
         }
-        Player player = event.getPlayer();
-        if (player == null) {
-            return;
+        if (event.getPacketType().equals(PacketType.Play.Client.CLOSE_WINDOW)) {
+            PacketContainer packet = event.getPacket();
+            int menuId = packet.getIntegers().read(0);
+            MenuManager.clearMenu(menuId);
         }
-        PacketContainer packet = event.getPacket();
-        int menuId = packet.getIntegers().read(0);
-        Menu menu = MenuManager.getMenu(menuId);
-        if (menu == null) {
-            return;
-        }
-        int slot = packet.getIntegers().read(1);
-        int button = packet.getIntegers().read(2);
-        if (slot < 0) {
-            return; // is drop, ignored.
-        }
-        String type = ((Enum<?>) packet.getStructures().read(1).getHandle()).name();
-        menu.handSlotClick(slot, button, ClickType.valueOf(type), player);
-        event.setCancelled(true);
     }
 }
