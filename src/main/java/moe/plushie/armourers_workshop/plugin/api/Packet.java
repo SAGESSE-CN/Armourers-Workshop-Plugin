e123c3db1912acff177167ff9109145262fabd77
SAGESSE <gdmmyzc@163.com>
2023-03-13 19:12:40 +0800

    Fix can't convert packet to bytes

--- src/main/java/moe/plushie/armourers_workshop/plugin/api/Packet.java
+++ src/main/java/moe/plushie/armourers_workshop/plugin/api/Packet.java
@@ -1,15 +1,18 @@
 package moe.plushie.armourers_workshop.plugin.api;
 
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+
 public class Packet<T> {
 
     private final byte[] bytes;
-    private final FriendlyByteBuf buf;
+    private final ByteBuf buffer;
     private final String channel;
 
     public Packet(String channel, FriendlyByteBuf buf) {
         this.channel = channel;
-        this.buf = buf;
-        this.bytes = buf.array();
+        this.buffer = Unpooled.copiedBuffer(buf);
+        this.bytes = buffer.array();
     }
 
     public String getChannel() {
